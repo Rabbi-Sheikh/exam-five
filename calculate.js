a3eb8652ab8519cbd7e1c71e2893bb93b767a782
Mohammad-Fojla-Rabby <fojlarabbisheikh@gmail.com>
2023-03-28 14:52:13 +0600

    blog.html added

--- calculate.js
+++ calculate.js
@@ -2,5 +2,11 @@ document.getElementById('triangle-button').addEventListener('click', function(){
     const a = toParseFloat('triangle-value-one');
     const b = toParseFloat('triangle-value-two');
     const totalArea = .5 * a * b ;
-     addElement('Triangle Area ---------'+' ' +  totalArea);
+     addElement('Triangle Area ---'+' ' +  totalArea);
+})
+document.getElementById('rectangle-button').addEventListener('click', function(){
+    const a = toParseFloat('rectangle-value-one');
+    const b = toParseFloat('rectangle-value-two');
+    const totalArea = a * b ;
+     addElement('Rectangle Area ---'+' ' +  totalArea);
 })
